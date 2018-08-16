8650a2c1adc552ac7eb79e2d903d66cbd342629c
Hítalo Silva <hitalos@gmail.com>
2018-08-16 00:12:06 -0300

    Update build dependencies

--- install-node.sh
+++ install-node.sh
@@ -13,4 +13,4 @@ curl -s -0 -L npmjs.org/install.sh | sh
 npm i -g yarn
 
 # Install build dependencies
-apk add autoconf automake g++ gcc libpng-dev make python
+apk add autoconf automake g++ gcc libpng-dev libtool make nasm python
